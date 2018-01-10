b367d91d1055ef9c26abd06533951de14ed57d14
Jan van Loenen <git@janvanloenen.nl>
2018-01-10 11:38:37 +0100

    support mbili and autonomo with ubee

--- examples/nbIOT_serial_passthrough/nbIOT_serial_passthrough.ino
+++ examples/nbIOT_serial_passthrough/nbIOT_serial_passthrough.ino
@@ -1,31 +1,55 @@
 #include <Arduino.h>
 
 #if defined(ARDUINO_AVR_LEONARDO)
+/* Arduino Leonardo + SODAQ NB-IoT Shield */
 #define DEBUG_STREAM Serial 
 #define MODEM_STREAM Serial1
 #define powerPin 7 
 
 #elif defined(ARDUINO_SODAQ_EXPLORER)
+/* SODAQ Explorer + SODAQ NB-IoT Shield */
 #define DEBUG_STREAM SerialUSB
 #define MODEM_STREAM Serial
 #define powerPin 7 
 
 #elif defined(ARDUINO_SAM_ZERO)
+/* Arduino Zero / M0 + SODAQ NB-IoT Shield */
 #define DEBUG_STREAM SerialUSB
 #define MODEM_STREAM Serial1
 #define powerPin 7 
 
+#elif defined(ARDUINO_SODAQ_AUTONOMO)
+/* SODAQ AUTONOMO + SODAQ NB-IoT Bee */
+#define DEBUG_STREAM SerialUSB
+#define MODEM_STREAM Serial1
+#define powerPin BEE_VCC
+#define enablePin BEEDTR
+
+#elif defined(ARDUINO_AVR_SODAQ_MBILI)
+/* SODAQ MBILI + SODAQ NB-IoT Bee */
+#define DEBUG_STREAM Serial
+#define MODEM_STREAM Serial1
+#define enablePin BEEDTR
+
 #else
-#error "Please select a Sodaq ExpLoRer, Arduino Leonardo, Arduino M0 (Crowduino M0) or add your board."
+#error "Please use one of the listed boards or add your board."
 #endif
 
 unsigned long baud = 9600;  //start at 9600 allow the USB port to change the Baudrate
 
 void setup() 
 {
-  // Turn the nb-iot module on
-  pinMode(powerPin, OUTPUT); 
-  digitalWrite(powerPin, HIGH);
+#ifdef powerPin
+    // Turn the nb-iot module on
+    pinMode(powerPin, OUTPUT);
+    digitalWrite(powerPin, HIGH);
+#endif
+
+#ifdef enablePin
+    // Set state to active
+    pinMode(enablePin, OUTPUT);
+    digitalWrite(enablePin, HIGH);
+#endif // enablePin
 
   // Start communication
   DEBUG_STREAM.begin(baud);
@@ -45,9 +69,11 @@ void loop()
     DEBUG_STREAM.write(MODEM_STREAM.read());
   }
   
+#ifndef ARDUINO_AVR_SODAQ_MBILI
   // check if the USB virtual serial wants a new baud rate
   if (DEBUG_STREAM.baud() != baud) {
     baud = DEBUG_STREAM.baud();
     MODEM_STREAM.begin(baud);
   }
+#endif // !ARDUINO_AVR_SODAQ_MBILI
 }
