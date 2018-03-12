72dc47da2d8bd3291d70a4e87c6ffd2c22430b3a
Tom van Diggelen <tom@actinius.com>
2018-03-12 17:49:04 +0100

    Split baudrate getters between N2 and R4, bump version to 1.4

--- src/Sodaq_nbIOT.h
+++ src/Sodaq_nbIOT.h
@@ -67,7 +67,9 @@ class Sodaq_nbIOT: public Sodaq_AT_Device
         
         // Returns the default baud rate of the modem.
         // To be used when initializing the modem stream for the first time.
-        uint32_t getDefaultBaudrate() { return 9600; };
+        uint32_t getSaraN2Baudrate() { return 9600; };
+        uint32_t getSaraR4Baudrate() { return 115200; };
+
         
         // Initializes the modem instance. Sets the modem stream and the on-off power pins.
         void init(Stream& stream, int8_t onoffPin, int8_t txEnablePin = -1, int8_t saraR4XXTogglePin = -1);
